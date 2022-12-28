e246c98165d70191b969c24d81288f6489f115a0
developer <viyadav@bestpeers.com>
2022-12-28 13:24:50 +0530

    include_prepend_extend

--- aa.rb
+++ aa.rb
@@ -1,16 +1,27 @@
-begin
-   
-    a=10
-    a.summ
-   1/0
-rescue NoMethodError=>e
-    p "#{e.class.name}"
+module A 
 
-rescue ZeroDivisionError=>e1
-    p "#{e1.class.name}"
-
-else
-    p "no exception"
-ensure
-    p "ensure runs"
+    def show
+        puts"this is module A"
+    end
 end
+module C    
+def show  
+puts "this is module C"
+end
+end
+module D    
+    def show  
+    puts "this is module C"
+    end
+    end
+class B   
+    include A 
+   prepend  C
+
+def show 
+    puts "this is class"
+end
+end
+p B.ancestors
+p B.new.show
+p B.show
